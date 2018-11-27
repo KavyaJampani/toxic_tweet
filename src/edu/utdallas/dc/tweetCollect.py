e090db864f43a977dae6e762a6324cb6ab1de7c1
Asish Kumar Gaddipati <gaddipati1991@gmail.com>
2018-11-27 17:56:04 -0500

    updates to tweetCollect

--- src/edu/utdallas/dc/tweetCollect.py
+++ src/edu/utdallas/dc/tweetCollect.py
@@ -10,8 +10,13 @@ consumer_key = "fg4pBbU44JUMRnDRbjtOmKCRR"
 consumer_secret = "iN3k4V7MuA7M12rwZOvmyAfJudd4qm4mnt68kSTJuOSkfP0K7X"
 
 
-def keywords():
-    fp = open("../../../../resources/keywords.txt", "r")
+tweets_file = None
+toxic_key_words_file = "../../../../resources/toxic-keywords.txt"
+non_toxic_key_words_file = "../../../../resources/non-toxic-keywords.txt"
+
+
+def keywords(key_words_file):
+    fp = open(key_words_file, "r")
     words = [w.strip() for w in fp.readlines() if w.strip() is not None and len(w.strip()) > 0]
     return words
 
@@ -25,7 +30,7 @@ class StdOutListener(StreamListener):
 
     def on_data(self, data):
         all_data = json.loads(data)
-        with open("../../../../resources/tweets2.json", 'a') as tf:
+        with open(tweets_file, 'a') as tf:
             tf.write('\n')
             # Write the json data directly to the file
             json.dump(all_data, tf)
@@ -37,11 +42,13 @@ class StdOutListener(StreamListener):
 
 if __name__ == '__main__':
     # This handles Twitter authentication and the connection to Twitter Streaming API
-    l = StdOutListener()
+    listener = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
+    stream = Stream(auth, listener)
 
     # This line filter Twitter Streams to capture data
-    stream.filter(languages=["en"],track = ["great","thanks","people","think","well","health"])
+    # change file name (keywords and output file) to track the respective
+    tweets_file = '../../../../resources/non-toxic-tweets.json'
+    stream.filter(languages=["en"], track=keywords(non_toxic_key_words_file))
 
